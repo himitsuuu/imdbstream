1e10e6ebbd94184531fda0ca11e4a1a3c9391b14
Venkat Nemana <venkat.nemana@gmail.com>
2019-03-10 14:06:55 -0700

    Initial Version

---
+++ imdb_stream_query.js
@@ -0,0 +1,7 @@
+var links = document.querySelector('#filmography > div:nth-child(2)')
+for (ii in links) {
+    console.log(links[ii])
+}
+    // console.log(links.querySelector("#actor-tt1158939"))
+    // let ii = document.querySelector(links[0])
+    // console.log(ii)
